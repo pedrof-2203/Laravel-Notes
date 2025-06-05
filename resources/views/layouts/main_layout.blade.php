8666670c4eed2fe0cd3dfa0dbe17eb8470556101
pedrof-2203 <pedroantoniofpa22@gmail.com>
2025-06-05 18:22:24 -0300

    basic layouts

---
+++ resources/views/layouts/main_layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Text 1 (TOP)</p>
+    <hr>
+
+    @yield('content')
+
+    <hr>
+    <p>Text 1 (BOTTOM)</p>
+</body>
+
+</html>
