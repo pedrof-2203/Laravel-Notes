0172d83312e574e89e95678b3e053dc70cebce10
pedrof-2203 <pedroantoniofpa22@gmail.com>
2025-06-16 12:46:02 -0300

    Aula 42

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>NOTES</title>
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png" type="image/png') }}">
 </head>
 
 <body>
-    <p>Text 1 (TOP)</p>
-    <hr>
 
     @yield('content')
 
-    <hr>
-    <p>Text 1 (BOTTOM)</p>
+    <script src="{{ asset('assets/bootstrap/bootstrap.bundle.min.js') }}"></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo 'Hello world';
-});
-
-Route::get('/about', function () {
-    echo 'About us';
-});
-
-Route::get('/main/{value}', [MainController::class, 'index']);
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-Route::get('/page3/{value}', [MainController::class, 'page3']);
+// auth routes
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
